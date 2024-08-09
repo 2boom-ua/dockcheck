2c4918cc9f8b5daf86b3cd4b7094f0bfac78f9ca
2boom <97739181+2boom-ua@users.noreply.github.com>
2024-08-09 09:37:46 +0300

    Update dockcheck.py

--- dockcheck.py
+++ dockcheck.py
@@ -11,37 +11,36 @@ from schedule import every, repeat, run_pending
 	
 
 def get_node_name():
-	node_name = ""
+	data = ""
 	try:
-		node_name = docker.from_env().info().get('Name')
+		data = docker.from_env().info().get('Name')
 	except docker.errors.DockerException as e:
 		print("Error:", e)
-	return node_name
+	return data
 
 
 def get_docker_counts():
-	docker_counts = {"volumes": 0, "images": 0, "networks": 0, "containers": 0}
 	try:
 		docker_client = docker.from_env()
-		docker_counts["volumes"] = len(docker_client.volumes.list())
-		docker_counts["images"] = len(docker_client.images.list())
-		docker_counts["networks"] = len(docker_client.networks.list())
-		docker_counts["containers"] = len(docker_client.containers.list())
-	except docker.errors.DockerException as e:
-		print("Error:", e)
-	except Exception as e:
-		print("Error:", e)
-	return docker_counts
+		return {
+			"volumes": len(docker_client.volumes.list()),
+			"images": len(docker_client.images.list()),
+			"networks": len(docker_client.networks.list()),
+			"containers": len(docker_client.containers.list())
+		}
+	except (docker.errors.DockerException, Exception) as e:
+		print(f"Error: {e}")
+		return {"volumes": 0, "images": 0, "networks": 0, "containers": 0}
 
 
-def get_docker_data(type_of_data):
-	return_data = []
+def get_docker_data(data_type: str):
+	data = []
 	try:
 		docker_client = docker.from_env()
-		if type_of_data == "networks":
+		if data_type == "networks":
 			networks = docker_client.networks.list()
-			if networks: [return_data.append(f"{network.name}") for network in networks]
-		elif type_of_data == "unetworks":
+			if networks: [data.append(f"{network.name}") for network in networks]
+		elif data_type == "unetworks":
 			used_networks = []
 			default_networks = ["none", "host"]
 			networks = docker_client.networks.list()
@@ -49,81 +48,74 @@ def get_docker_data(type_of_data):
 			for container in containers:
 				[used_networks.append(network) for network in container.attrs['NetworkSettings']['Networks']]
 			unused_networks = [network for network in networks if network.name not in used_networks and network.name not in default_networks]
-			if unused_networks: [return_data.append(f"{network.name} {network.short_id}") for network in unused_networks]
-		elif type_of_data == "images":
+			if unused_networks: [data.append(f"{network.name} {network.short_id}") for network in unused_networks]
+		elif data_type == "images":
 			images = docker_client.images.list()
 			if images:
 				for image in images:
 					imagename = ''.join(image.tags).split(':')[0].split('/')[-1]
 					if imagename == '': imagename = image.short_id.split(':')[-1]
-					return_data.append(f"{image.short_id.split(':')[-1]} {imagename}")
-		elif type_of_data == "containers":
+					data.append(f"{image.short_id.split(':')[-1]} {imagename}")
+		elif data_type == "containers":
 			for container in docker_client.containers.list(all=True):
 				container_info = docker_client.api.inspect_container(container.id)
 				if "State" in container_info and "Health" in container_info["State"]:
-					return_data.append(f"{container.name} {container.status} {container.attrs['State']['Health']['Status']} {container.short_id}")
+					data.append(f"{container.name} {container.status} {container.attrs['State']['Health']['Status']} {container.short_id}")
 				else:
-					return_data.append(f"{container.name} {container.status} {container.attrs['State']['Status']} {container.short_id}")
+					data.append(f"{container.name} {container.status} {container.attrs['State']['Status']} {container.short_id}")
 		else:
-			if type_of_data == "volumes":
+			if data_type == "volumes":
 				volumes = docker_client.volumes.list()
 			else:
 				volumes = docker_client.volumes.list(filters={"dangling": "true"})
-			if volumes: [return_data.append(f"{volume.short_id}") for volume in volumes]
-	except docker.errors.DockerException as e:
-		print("Error:", e)
-	except Exception as e:
-		print("Error:", e)
-	return return_data
+			if volumes: [data.append(f"{volume.short_id}") for volume in volumes]
+	except (docker.errors.DockerException, Exception) as e:
+		print(f"Error: {e}")
+	return data
 
 
-def send_message(message : str):
+def send_message(message: str):
+	def send_request(url, json_data=None, data=None, headers=None):
+		try:
+			response = requests.post(url, json=json_data, data=data, headers=headers)
+			response.raise_for_status()
+		except requests.exceptions.RequestException as e:
+			print(f"Error sending message: {e}")
 	if telegram_on:
-		try:
-			for telegram_token, telegram_chat_id in zip(telegram_tokens, telegram_chat_ids):
-				response = requests.post(f"https://api.telegram.org/bot{telegram_token}/sendMessage", json = {"chat_id": telegram_chat_id, "text": message, "parse_mode": "Markdown"})
-				response.raise_for_status()
-		except requests.exceptions.RequestException as e:
-			print(f"Error sending Telegram message: {e}")
+		for token, chat_id in zip(telegram_tokens, telegram_chat_ids):
+			url = f"https://api.telegram.org/bot{token}/sendMessage"
+			json_data = {"chat_id": chat_id, "text": message, "parse_mode": "Markdown"}
+			send_request(url, json_data)
 	if discord_on:
-		try:
-			for discord_token in discord_tokens:
-				response = requests.post(f"https://discord.com/api/webhooks/{discord_token}", json = {"content": message.replace("*", "**")})
-				response.raise_for_status()
-		except requests.exceptions.RequestException as e:
-			print(f"Error sending Discord message: {e}")
+		for token in discord_tokens:
+			url = f"https://discord.com/api/webhooks/{token}"
+			json_data = {"content": message.replace("*", "**")}
+			send_request(url, json_data)
 	if slack_on:
-		try:
-			for slack_token in slack_tokens:
-				response = requests.post(f"https://hooks.slack.com/services/{slack_token}", json = {"text": message})
-				response.raise_for_status()
-		except requests.exceptions.RequestException as e:
-			print(f"Error sending Slack message: {e}")
+		for token in slack_tokens:
+			url = f"https://hooks.slack.com/services/{token}"
+			json_data = {"text": message}
+			send_request(url, json_data)
 	message = message.replace("*", "")
 	header, message = message.split("\n", 1)
 	message = message.strip()
 	if gotify_on:
-		try:
-			for gotify_chat_web, gotify_token in zip(gotify_chat_webs, gotify_tokens):
-				response = requests.post(f"{gotify_chat_web}/message?token={gotify_token}", json={'title': header, 'message': message, 'priority': 0})
-				response.raise_for_status()
-		except requests.exceptions.RequestException as e:
-			print(f"Error sending Gotify message: {e}")
+		for token, chat_url in zip(gotify_tokens, gotify_chat_urls):
+			url = f"{chat_url}/message?token={token}"
+			json_data = {'title': header, 'message': message, 'priority': 0}
+			send_request(url, json_data)
 	if ntfy_on:
-		try:
-			for ntfy_chat_web, ntfy_token in zip(ntfy_chat_webs, ntfy_tokens):
-				response = requests.post(f"{ntfy_chat_web}/{ntfy_token}", data = message.encode(encoding = 'utf-8'), headers = {"title": header})
-				response.raise_for_status()
-		except requests.exceptions.RequestException as e:
-			print(f"Error sending Ntfy message: {e}")
+		for token, chat_url in zip(ntfy_tokens, ntfy_chat_urls):
+			url = f"{chat_url}/{token}"
+			data_data = message.encode(encoding = 'utf-8')
+			headers_data = {"title": header}
+			send_request(url, None, data_data, headers_data)
 	if pushbullet_on:
-		try:
-			for pushbullet_token in pushbullet_tokens:
-				response = requests.post('https://api.pushbullet.com/v2/pushes',\
-				json = {'type': 'note', 'title': header, 'body': message}, headers = {'Access-Token': pushbullet_token, 'Content-Type': 'application/json'})
-				response.raise_for_status()
-		except requests.exceptions.RequestException as e:
-			print(f"Error sending Pushbullet message: {e}")
+		for token in pushbullet_tokens:
+			url = "https://api.pushbullet.com/v2/pushes"
+			json_data = {'type': 'note', 'title': header, 'body': message}
+			headers_data = {'Access-Token': token, 'Content-Type': 'application/json'}
+			send_request(url, json_data, None, headers_data)
 
 
 if __name__ == "__main__":
@@ -138,36 +130,25 @@ if __name__ == "__main__":
 		with open(f"{current_path}/config.json", "r") as file:
 			parsed_json = json.loads(file.read())
 		sec_repeat = int(parsed_json["SEC_REPEAT"])
-		telegram_on, discord_on, gotify_on = parsed_json["TELEGRAM"]["ON"], parsed_json["DISCORD"]["ON"], parsed_json["GOTIFY"]["ON"]
-		ntfy_on, pushbullet_on, slack_on = parsed_json["NTFY"]["ON"], parsed_json["PUSHBULLET"]["ON"], parsed_json["SLACK"]["ON"]
-		if telegram_on:
-			telegram_tokens, telegram_chat_ids = parsed_json["TELEGRAM"]["TOKENS"], parsed_json["TELEGRAM"]["CHAT_IDS"]
-			monitoring_mg += "- messenging: Telegram,\n"
-		if discord_on:
-			discord_tokens = parsed_json["DISCORD"]["TOKENS"]
-			monitoring_mg += "- messenging: Discord,\n"
-		if slack_on:
-			slack_tokens = parsed_json["SLACK"]["TOKENS"]
-			monitoring_mg += "- messenging: Slack,\n"
-		if gotify_on:
-			gotify_tokens, gotify_chat_webs = parsed_json["GOTIFY"]["TOKENS"], parsed_json["GOTIFY"]["CHAT_WEB"]
-			monitoring_mg += "- messenging: Gotify,\n"
-		if ntfy_on:
-			ntfy_tokens, ntfy_chat_webs = parsed_json["NTFY"]["TOKENS"], parsed_json["NTFY"]["CHAT_WEB"]
-			monitoring_mg += "- messenging: Ntfy,\n"
-		if pushbullet_on:
-			pushbullet_tokens = parsed_json["PUSHBULLET"]["TOKENS"]
-			monitoring_mg += "- messenging: Pushbullet,\n"
+		telegram_on, discord_on, gotify_on, ntfy_on, pushbullet_on, slack_on = (parsed_json[key]["ON"] for key in ["TELEGRAM", "DISCORD", "GOTIFY", "NTFY", "PUSHBULLET", "SLACK"])
+		services = {
+		"TELEGRAM": ["TOKENS", "CHAT_IDS"], "DISCORD": ["TOKENS"], "SLACK": ["TOKENS"],
+		"GOTIFY": ["TOKENS", "CHAT_URLS"], "NTFY": ["TOKENS", "CHAT_URLS"], "PUSHBULLET": ["TOKENS"]
+		}
+		for service, keys in services.items():
+			if parsed_json[service]["ON"]:
+				globals().update({f"{service.lower()}_{key.lower()}": parsed_json[service][key] for key in keys})
+				monitoring_mg += f"- messaging: {service.capitalize()},\n"
 		for type_of in docker_counts:
 			monitoring_mg += f"- monitoring: {docker_counts[type_of]} {type_of}\n"
-		send_message(f"{header_message}{monitoring_mg}- polling period: {sec_repeat} seconds.")
+		send_message(f"{header_message}{monitoring_mg}- polling period: {sec_repeat} seconds.")	
 	else:
 		print("config.json not found")
 
 
 @repeat(every(sec_repeat).seconds)
 def docker_checker():
-	#docker-image
+	# docker-image
 	global old_list_images
 	status_dot = yellow_dot
 	message, header_message = "", f"*{nodename}* (docker.images)\n"
@@ -203,7 +184,7 @@ def docker_checker():
 			send_message(f"{header_message}{message}")
 
 
-	#docker-volume.network
+	# docker-volume.network
 	check_types = ["volumes", "networks"]
 	global old_list_networks
 	global old_list_volumes
@@ -237,7 +218,7 @@ def docker_checker():
 				send_message(f"{header_message}{message}")
 
 
-	#docker-unused.volumes.networks
+	# docker-unused.volumes.networks
 	check_types = ["volumes", "networks"]
 	global old_list_uvolumes
 	global old_list_unetworks
@@ -269,7 +250,7 @@ def docker_checker():
 				send_message(f"{header_message}{message}")
 
 
-	#docker-container
+	# docker-container
 	global old_list_containers
 	status_dot = orange_dot
 	message, header_message = "", f"*{nodename}* (docker.containers)\n"
